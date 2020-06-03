0939ccf71ddfea1780f0a5287bb879daea4fe69f
MohammadHosein Forghani <mohofo7@gmail.com>
2020-06-03 23:00:55 +0430

    feat: add product page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "../styles/global.scss";
 import Layout from "../components/Layout";
+import "../styles/global.scss";
 
 const MyApp = ({ Component, pageProps }) => (
     <Layout>
--- pages/product/[id]/index.tsx
+++ pages/product/[id]/index.tsx
@@ -3,23 +3,37 @@ import { Carousel } from "antd";
 import styles from "./style.module.scss";
 
 const Product = (props) => {
-    const { id } = props;
+    const { product } = props;
+    const { images, title, price } = product;
     return (
-        <Carousel autoplay>
-            <div>
-                <img src="http://kia-gallery.com/Images/Gallery/SProductT_6685.jpg" alt="1"/>
+        <div className={styles.page}>
+            <Carousel className={styles.carousel}>
+                {images.map(image => (
+                    <div className="ann">
+                        <div className={styles.sliderItem} style={{ backgroundImage: `url(${image})` }}/>
+                    </div>
+                ))}
+            </Carousel>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <div className={styles.title}>{title}</div>
+                    <div className={styles.price}>{price}</div>
+                </div>
             </div>
-            <div>
-                <img src="http://kia-gallery.com/Images/Gallery/SProductT_6685.jpg" alt="1"/>
-            </div>
-        </Carousel>
+        </div>
     );
 };
 
 export const getServerSideProps = async (context) => {
     console.log(context.params);
     return {
-        props: { id: context.params.id },
+        props: {
+            product: {
+                images: ['http://kia-gallery.com/Images/Gallery/SProductT_6685.jpg', 'http://kia-gallery.com/Images/Gallery/SProductT_1165.jpg'],
+                title: 'سنجاق سینه پرنده برنجی',
+                price: 100000
+            }
+        },
     }
 }
 
